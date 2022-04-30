d37508e32e5c81be6096d42b56cfab715a13635c
Thiago Santos <thiagop.santos182@gmail.com>
2022-04-30 08:43:03 -0400

    Adding middleware to check if account exists

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found' });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post('/accounts', (request, response) => {
   const { cpf, name } = request.body;
 
@@ -29,14 +43,8 @@ app.post('/accounts', (request, response) => {
   return response.status(201).json(account);
 });
 
-app.get('/statement', (request, response) => {
-  const { cpf } = request.headers;
-  
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: 'Customer not found' });
-  }
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
 
   return response.json(customer.statement);
 });
