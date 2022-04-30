46f94ba62f30993764d2cb6274c0f0595a58f59b
Thiago Santos <thiagop.santos182@gmail.com>
2022-04-30 08:29:01 -0400

    Adding create Account and list Statement routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
 const express = require('express');
+const { v4: uuidV4 } = require('uuid');
 
 const app = express();
 app.use(express.json());
 
-app.listen(3333);
+const customers = [];
+
+app.post('/accounts', (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(customer => customer.cpf === cpf);
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({
+      'error': 'Customer already exists'
+    });
+  }
+
+  const account = {
+    id: uuidV4(),
+    name,
+    cpf,
+    statement: [],
+  }
+
+  customers.push(account);
+
+  return response.status(201).json(account);
+});
+
+app.get('/statement', (request, response) => {
+  const { cpf } = request.headers;
+  
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: 'Customer not found' });
+  }
+
+  return response.json(customer.statement);
+});
+
+app.listen(3333, () => {
+  console.log('Server is running 🚀');
+});
